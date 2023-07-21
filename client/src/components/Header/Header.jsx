a92d15b85e38bcdc3882a90bba46f5759b3ebc21
Chelsea Quindipan <quindipc@gmail.com>
2023-07-20 21:07:41 -0400

    edit header scss

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,31 +10,28 @@ import "./Header.scss";
 // Pages
 
 export default function Header() {
-
-
   return (
     <header className="header">
       <div className="header__logo">
-        {/* <img src="" alt="Google Logo" /> */}
+        <img src="" alt="Google Logo" />
       </div>
       <div className="header__searchbar">
         <form action="">
-          <input
-            className="header__search"
-            type="text"
-            placeholder="Search"
-          />
+          <input className="header__search" type="text" placeholder="Search" />
           <button type="submit" className="header__button">
             Search
           </button>
-              </form>
-          </div>
-          <div className="header__edit">
-              {/* <img className="header__edit-icon" src="" alt="Gear Icon" /> */}
-          </div>
-          <div className="header__avatar">
-              {/* <img className="header__avatar-img" src="" alt="Avatar" /> */}
-          </div>
+        </form>
+      </div>
+
+      <div className="header__profile-settings">
+        <div className="header__edit">
+          <img className="header__edit-icon" src="" alt="Gear Icon" />
+        </div>
+        <div className="header__avatar">
+          <img className="header__avatar-img" src="" alt="Avatar" />
+        </div>
+      </div>
     </header>
   );
 }
