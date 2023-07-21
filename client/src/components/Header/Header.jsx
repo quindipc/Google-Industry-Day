711359de7c5aa5b425f0c708cf23c15258b6fa10
Chelsea Quindipan <quindipc@gmail.com>
2023-07-20 22:56:25 -0400

    add divider & divider styles mixins

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,41 +16,48 @@ import MoreIcon from "../../assets/images/icons/threedots.svg";
 // Pages
 
 export default function Header() {
-
   const handleSearchSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
-    <header className="header">
-      <div className="header__logo-container">
-        <Link to="/" className="header__logo-index">
-          <img
-            className="header__logo--tablet"
-            src={GoogleLogo}
-            alt="Google Logo"
-          />
-        </Link>
-        <img className="header__menu" src={MenuIcon} alt="menu" />
-        <Link to="/" className="header__logo-index">
-          <img
-            className="header__logo--mobile"
-            src={GoogleLogo}
-            alt="Google Logo"
-          />
-        </Link>
-        <img className="header__avatar--mobile" src={Avatar} alt="Avatar" />
-      </div>
+    <>
+      <header className="header">
+        <div className="header__logo-container">
+          <Link to="/" className="header__logo-index">
+            <img
+              className="header__logo--tablet"
+              src={GoogleLogo}
+              alt="Google Logo"
+            />
+          </Link>
+          <img className="header__menu" src={MenuIcon} alt="menu" />
+          <Link to="/" className="header__logo-index">
+            <img
+              className="header__logo--mobile"
+              src={GoogleLogo}
+              alt="Google Logo"
+            />
+          </Link>
+          <img className="header__avatar--mobile" src={Avatar} alt="Avatar" />
+        </div>
 
-      <form className="header__searchbar-container" onSubmit={handleSearchSubmit}>
-        <input className="header__search" type="text" placeholder="Search" />
-      </form>
+        <form
+          className="header__searchbar-container"
+          onSubmit={handleSearchSubmit}
+        >
+          <input className="header__search" type="text" placeholder="Search" />
+        </form>
 
-      <div className="header__profile-settings">
-        <img className="header__edit-icon" src={GearIcon} alt="Gear Icon" />
-        <img className="header__edit-icon" src={MoreIcon} alt="Gear Icon" />
-        <img className="header__avatar--tablet" src={Avatar} alt="Avatar" />
+        <div className="header__profile-settings">
+          <img className="header__edit-icon" src={GearIcon} alt="Gear Icon" />
+          <img className="header__edit-icon" src={MoreIcon} alt="Gear Icon" />
+          <img className="header__avatar--tablet" src={Avatar} alt="Avatar" />
+        </div>
+      </header>
+      <div className="header__divider-container">
+        <hr className="header__divider" />
       </div>
-    </header>
+    </>
   );
 }
